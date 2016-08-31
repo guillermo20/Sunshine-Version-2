84b4c66e9efb4a1eed919a839b7cfaa337d9a3d7
Guillermo <guillermo20@gmail.com>
2016-08-30 20:20:05 -0430

    added onOptionsItemSelected to the forecastFragment

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -57,13 +60,26 @@ public class ForecastFragment extends Fragment {
 
     }
 
-
-    /*@Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.forecastfragment,menu);
-        //super.onCreateOptionsMenu(menu, inflater);
-    }*/
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId= item.getItemId();
+        switch (itemId){
+            case R.id.action_refresh: return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     public class FetchWeatherTask extends AsyncTask<Void,Void,String>{
 
